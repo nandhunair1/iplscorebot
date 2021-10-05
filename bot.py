ba81c2b46b2f57ccef3dd8275521bd26031a9251
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-10-05 23:30:02 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,43 @@
+"""
+    IPL Score Telegram Bot
+    Copyright (C) 2021 @ImJanindu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+import logging
+import iplscore
+from pyrogram import Client, filters, idle
+from vars import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client(
+    "iplscore",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@bot.on_message(filters.private & filters.command("start"))
+async def score(_, message):
+    m = await message.reply_text("`Gathering ongoing ipl match scorecard...`")
+    try:
+        a = iplscore.score()
+        return await m.edit(a)
+    except:
+        return await m.edit("`No any ongoing ipl matches at this time.`")
+
+
+bot.start()
+idle()
