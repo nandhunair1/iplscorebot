160facafb4b02760da3961a00916f29b86517c10
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-10-06 20:49:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,9 +47,9 @@ async def score(_, message):
         hello = lol.split(None, 1)[1].strip()
         hmm = re.findall('[A-Z][^A-Z]*', hello)
         text = ""
-        text = text + "**🔴 𝐋𝐈𝐕𝐄 𝐈𝐏𝐋 𝐒𝐂𝐎𝐑𝐄 🏏**\n\n" + f"**{match_descrition[1].text}**" + "\n\n" + f"**{status[0].text}**" + "\n\n" + f"**⬇️ Scorecard ⬇️** \n\n**{lol.split(None, 1)[0].strip()} {hmm[0]}**\n\n**{hmm[1]}{hmm[2]}**" + "\n\n" + "**Bot by:** <a href='https://t.me/Infinity_Bots'>**Infinity Bots**</a>\n**Developer:** <a href='https://github.com/ImJanindu'>**Janindu**</a>"
+        text = text + "**🔴 𝐋𝐈𝐕𝐄 𝐈𝐏𝐋 𝐒𝐂𝐎𝐑𝐄 🏏**\n\n" + f"**{match_descrition[1].text}**" + "\n\n" + f"**{status[0].text}**" + "\n\n" + f"**⬇️ Scorecard ⬇️** \n\n=> **{lol.split(None, 1)[0].strip()} {hmm[0]}**\n\n**=> {hmm[1]}{hmm[2]}**" + "\n\n" + "**Bot by:** <a href='https://t.me/Infinity_Bots'>**Infinity Bots**</a>\n**Developer:** <a href='https://github.com/ImJanindu'>**Janindu**</a>"
         text = text.replace("(", " (")
-        text = text.replace(")", ")\n**Runs:** ")
+        text = text.replace(")", ")\nRuns: ")
         await m.edit(text, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(
                                 [[InlineKeyboardButton(
                                      "Refresh 🔁", url="https://t.me/iplscorerobot?start=true")]]))
